32ec6c0b13395413cf0cae79d26f2850d9cad2fe
Niklas <jan-niklas-bremer@web.de>
2025-10-27 11:09:44 +0100

    More Implementing

--- NewsDisplay.xaml.cs
+++ NewsDisplay.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Project_NNTP_Niklas
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewsDisplay.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewsDisplay : Window
     {
-        public MainWindow()
+        public NewsDisplay()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void BtnLogin_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
